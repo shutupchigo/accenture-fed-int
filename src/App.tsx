73d9f43751bf4b06ec87220d126c284ea3c69eef
Sunil Patel <sunil@strong.digital>
2023-02-21 11:19:06 +1000

    product dropdown filter added

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import './App.css';
 import { ProductCard } from './components/ProductCard/ProductCard';
-
+import { ProductFilter } from './components/ProductFilter/ProductFilter';
 
 function App() {
 
+
+
   const [products, setProducts] = useState([]);
+  const [filteredProducts, setfilteredProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [hasError, setHasError] = useState(false);
+  const [filterText, setFilterText] = useState('');
 
   useEffect(() => {
     setIsLoading(true);
@@ -17,6 +21,7 @@ function App() {
       .then(response => response.json())
       .then(data => {
         setProducts(data)
+        setfilteredProducts(data)
       })
       .catch(error => {
         console.error(error)
@@ -25,11 +30,43 @@ function App() {
     setIsLoading(false);
   }, []);
 
+  const filteredProductList = (arr: never[], type: string) => {
+
+    /** return only requested product */
+    let products = arr.filter((item) => {
+      if (type) {
+        return item['type'] == type
+      }
+    })
+
+    /** Return all products */
+    if (type == 'all') {
+      products = arr;
+    }
+
+    return products;
+  }
+
+  /** Prep data for dropdown product types in child component */
+  const productTypes: string[] = [...new Set(products.map(item => item['type']))];
+
+  const onTypeValueSelected = (filteredValue: string) => {
+    setFilterText(filteredValue);
+    setfilteredProducts(filteredProductList(products, filteredValue));
+  }
+
+
   return (
     <main>
-      {products &&
-        <ul className='product__container--outer'>
-          {products.map(item => (
+      <div className="container">
+        <h1 className='text-center'>Products Finder</h1>
+      </div>
+      <div className="product__container--filter">
+        <ProductFilter productTypes={productTypes} typeValueSelected={onTypeValueSelected} />
+      </div>
+      {filteredProducts &&
+        <ul className="product__container--outer">
+          {filteredProducts.map(item => (
             <li key={item['index']} className="product__container--inner">
               <ProductCard
                 productName={item['productName']}
---
+++ src/components/ProductFilter/ProductFilter.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+type ProductFilterProps = {
+    productTypes?: string[],
+    typeValueSelected: any
+}
+
+
+{/*  Helper Function*/ }
+function capitalizeFirstLetter(string: string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export const ProductFilter = (props: ProductFilterProps) => {
+    const productTypes = props.productTypes;
+
+    const onTypeValueChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        props.typeValueSelected(e.target.value)
+    }
+
+    return (
+        <>
+            <span>Filter By &nbsp;</span>
+            {productTypes &&
+                <select name="productTypeFilter" onChange={onTypeValueChange} >
+                    <option value="all">Type</option>
+                    {productTypes.map(item => (
+                        <option key={item} value={item}>{capitalizeFirstLetter(item)}</option>
+                    ))}
+                </select>
+            }
+        </>
+    )
+}
